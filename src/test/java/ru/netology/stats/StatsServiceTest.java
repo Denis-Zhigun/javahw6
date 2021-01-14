3eab224e5a6f5cd6a0d0584c9fb333136d464fff
Denis Zhigun <zhigundv@gmail.com>
2021-01-14 21:41:01 +0300

    Update StatsService

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,18 +1,36 @@
 package ru.netology.stats;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
+
     @Test
-    void calculateSum() {
-        StatisticsService service = new StatisticsService();
+        void calculateSum() {
+            StatsService service = new StatsService();
+
+            long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+            long expected = 180;
+
+            long actual = service.calculateSum(purchases);
+
+            assertEquals(expected, actual);
+        }
+
+   @Test
+    void findMax() {
+        StatsService service = new StatsService();
 
         long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 180;
+        long expected = 20;
 
-        long actual = service.calculateSum(purchases);
+        long actual = service.findMax(purchases);
 
         assertEquals(expected, actual);
     }
 
-}
+
+    }
+
+
